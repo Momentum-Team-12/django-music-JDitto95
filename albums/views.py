c4dba0acf8a2283bdca117c3fe983f0dd090f70f
JDitto95 <powell0295@gmail.com>
2022-05-05 17:05:07 -0400

    working on it

--- albums/views.py
+++ albums/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+
+import albums
 from .models import Album
 from .forms import AlbumForm
 # Create your views here.
@@ -6,5 +8,32 @@ def list_albums(request):
     albums = Album.objects.all()
     return render(request, "albums/list_albums.html", {"albums": albums})
 
+def album_detail(request, pk):
+    contact = get_object_or_404(Album, pk=pk)
+    # notes = Note.objects.filter(contact_id=contact.pk)
+    form = AlbumForm()
+    return render(request, "albums/album_detail.html", {"albums": albums })
 
+def add_albums(request):
+    if request.method == 'GET':
+        form = AlbumForm()
+    else:
+        form = AlbumForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_albums')
+            
+    return render( request, "albums/add_album.html", {"form": form})
+
+# def albums(request, pk):
+#     contact = get_object_or_404(Album, pk=pk)
+#     if request.method == 'GET':
+#         form = AlbumForm()
+#     else:
+#         form = AlbumForm(data=request.POST)
+#         if form.is_valid():
+#             note = form.save(commit=False)
+#             note.contact = contact
+#             note.save()
+#             return redirect(to='list_albums', pk=pk)
     
--- django_music/urls.py
+++ django_music/urls.py
@@ -20,4 +20,5 @@ from albums import views as albums_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',albums_views.list_albums, name='list_album'),
+    path('albums/add/', albums_views.add_albums, name='add_album'),
 ]
